5dea67752697fbdd7bc34a5fadae2ff7cc960b5c
tsm3 <tristan.slayde.mansfield@gmail.com>
2020-09-25 06:03:54 -0500

    maybe all? probs bug

--- elec326-hwk3/sm_adder_s.v
+++ elec326-hwk3/sm_adder_s.v
@@ -1,16 +1,16 @@
-module ripple_carry_adder(input wire [n-1:0]x, y, input wire cin, output wire [n-1:0] Sum, output wire Cout);  // Parameterized Ripple Carry Adder
-   parameter n = 8;
+module ripple_carry_adder(input wire [n-1:0]x, y, input wire cin, output wire [n-1:0] Sum, output wire Cout);  // Parameterized Ripple Carry Adder - 8 bit
+   parameter n = 4;
    assign {Cout, Sum} = x + y + cin;
 endmodule // ripple_carry_adder
 
-module comparator(input wire [n-1:0] x, y,  output wire EQ, GT);  // Parametrized Comparator
-   parameter n = 8;
+module comparator(input wire [n-1:0] x, y,  output wire EQ, GT);  // Parametrized Comparator - 8 bit
+   parameter n = 4;
    assign EQ = (x == y);
    assign GT = (x > y);
 endmodule // comparator
 
 module mux2(input wire c, input wire [n-1:0] a,b, output wire [n-1:0] X); // Parameterized 2-input n-bit MUX
-   parameter n = 1;
+   parameter n = 4;
    assign X = (c) ? a : b;
 endmodule // mux2
 
@@ -19,6 +19,33 @@ module sm_adder_s( input wire [4:0] a, b, output wire [4:0] SUM, output wire OVF
 
    // Instantiate the modules defined above as required by your schematic.
    // Glue modules together using simple assign statements to implement gate logic.
+
+   wire[3:0] tempsum;
+   wire[3:0] X2, Y2;
+   wire[3:0] bruh;
+   
+
+   wire EQ, GT, A4;
+   wire cIN;
+   wire X4cIN, Y4cIN;
+
+   
+   comparator comp(a[3:0], b[3:0], EQ, GT);
+   mux2 muxSign(GT, a[4], b[4], A4);
+   assign SUM[4] = A4 & ~EQ;
+
+
+   assign cIN = a[4] ^ b[4];
+   assign X4cIN = cIN & a[4];
+   assign Y4cIN = cIN & b[4];
+   
+   mux2 muxX(X4cIN, a[3:0], ~a[3:0], X2);
+   mux2 muxY(Y4cIN, b[3:0], ~b[3:0], Y2);
+   ripple_carry_adder add(X2, Y2, cIN, SUM[3:0], OVFLW);
+   
+
+
+
 
  
 endmodule // sm_adder
