b4dbf7178a44d72dd034a8b0081ea2763113ab25
tsm3 <tristan.slayde.mansfield@gmail.com>
2020-10-07 07:44:14 -0500

    this supposed to be simple yeah?

--- elec326-project1/branch/branch.v
+++ elec326-project1/branch/branch.v
@@ -9,8 +9,18 @@ input [15:0] reg1_data_pi,
 input [15:0] reg2_data_pi,
 input        alu_carry_bit_pi,
 
-output  is_branch_taken_po)
-;
+output  is_branch_taken_po);
 
+wire eq;
+wire ge;
+wire le;
+wire carry;
+
+assign eq = branch_eq_pi & (reg1_data_pi == reg2_data_pi); // Only one of the pi signals will be 1 at a time, so either 0 or 1 OR terms are ever 1
+assign ge = branch_ge_pi & (reg1_data_pi >= reg2_data_pi);
+assign le = branch_le_pi & (reg1_data_pi <= reg2_data_pi);
+assign carry = branch_carry_pi & alu_carry_bit_pi;
+
+assign is_branch_taken_po = eq | ge | le | carry;
 
 endmodule // branch_comparator
