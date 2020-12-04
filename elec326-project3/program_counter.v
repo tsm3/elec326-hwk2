448733c4add895c5515ac1ab23893df829572a58
tsm3 <tristan.slayde.mansfield@gmail.com>
2020-12-04 06:34:25 -0600

    don't increment on a reset

--- elec326-project3/program_counter.v
+++ elec326-project3/program_counter.v
@@ -28,18 +28,19 @@ module program_counter (
 		if (reset_pi) begin
 			//Reset stuff
 			PC <= 16'd0; // Make sure this is the right thing
-		end else if (clk_en_pi) begin
-			 if (branch_taken_pi) begin
-				//Branch taken stuff // Includes sign extension
-				PC <= PC + {{10{branch_immediate_pi[5]}}, branch_immediate_pi[5:0]}; // Unsure about this line, but it should be right
-			end else if (jump_taken_pi) begin
-				//Jump taken stuff // Includes sign extension
-				PC <= PC + {{4{jump_immediate_pi[11]}}, jump_immediate_pi[11:0]};
-			end 
-			
-			PC <= PC + 16'd2; // If none of ^^ , increment by 2
-			
-		end
+		end else begin // If not reset vv
+			if (clk_en_pi) begin
+				if (branch_taken_pi) begin
+					//Branch taken stuff // Includes sign extension
+					PC <= PC + {{10{branch_immediate_pi[5]}}, branch_immediate_pi[5:0]}; // Unsure about this line, but it should be right
+				end else if (jump_taken_pi) begin
+					//Jump taken stuff // Includes sign extension
+					PC <= PC + {{4{jump_immediate_pi[11]}}, jump_immediate_pi[11:0]};
+				end 
+				
+				PC <= PC + 16'd2; // If none of ^^ , increment by 2
+			end
+		end // From reset's else
 	end
    
 	assign pc_po = PC;
